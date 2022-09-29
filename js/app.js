539f0ea15185118f7c4e17247e629c312e6aff7c
Ian Skelskey <ianskelskey@gmail.com>
2022-09-28 23:35:52 -0400

    Add borders to squares dynamically on click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-const board = document.querySelector(".board")
-
-document.addEventListener("DOMContentLoaded", showCoffees)
+const blackSquares = document.querySelectorAll(".black-square")
+const whiteSquares = document.querySelectorAll(".white-square")
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
@@ -10,3 +9,17 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err))
   })
 }
+
+blackSquares.forEach(square => {
+  square.addEventListener('click', function handleClick(event) {
+    console.log('black square clicked', event);
+    square.setAttribute('style', 'border-color: #e74c3c; border-style: solid; border-width: 4pt;')
+  })
+})
+
+whiteSquares.forEach(square => {
+  square.addEventListener('click', function handleClick(event) {
+    console.log('black square clicked', event);
+    square.setAttribute('style', 'border-color: #0000ff; border-style: solid; border-width: 4pt;')
+  })
+})
