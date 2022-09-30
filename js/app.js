ac7061ad7c2621d54deddd7195316cb89836b721
Ian Skelskey <ianskelskey@gmail.com>
2022-09-30 07:50:23 -0400

    Start to implement oop and game pieces

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ if ("serviceWorker" in navigator) {
     .then(res => console.log("service worker registered"))
     .catch(err => console.log("service worker not registered", err))
 }
+
+include('js/ui.js');
+
+function include(file) {
+  const script = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+  document.getElementsByTagName('head').item(0).appendChild(script);
+}
--- js/ui.js
+++ js/ui.js
@@ -1,36 +1,70 @@
-const board = document.getElementById("board");
+class Board {
+  constructor(size) {
+    this.board = document.createElement('div');
+    this.board.id = 'board';
+    this.createGrid(size);
+    this.addToDOM('main');
+    this.squares = document.querySelectorAll('.square');
+    this.setupSquareClickEvents();
+    this.createPieces();
+  }
 
-createBoard();
+  addToDOM(elementID) {
+    const element = document.getElementById(elementID);
+    element.appendChild(this.board);
+  }
 
-const squares = document.querySelectorAll(".square")
-
-function highlightSquare(square) {
-  square.setAttribute('style', 'border-color: #e74c3c; border-style: solid; border-width: 4pt;');
-}
-
-function createSquare(color) {
-  let square = document.createElement('div')
-  square.classList.add('square');
-  square.classList.add(color + '-square');
-  return square;
-}
-
-function createBoard() {
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if ((i+j) % 2 === 0) {
-        board.appendChild(createSquare('black'));
-      } else {
-        board.appendChild(createSquare('white'));
+  createGrid(size) {
+    for (let i = 0; i < size; i ++) {
+      for (let j = 0; j < size; j++) {
+        if ((i+j) % 2 === 0) {
+          this.board.appendChild(this.createSquare('black'));
+        } else {
+          this.board.appendChild(this.createSquare('white'));
+        }
       }
     }
   }
-  console.log('Board created!');
+
+  createSquare(color) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.classList.add(color + '-square');
+    return square;
+  }
+
+  setupSquareClickEvents() {
+    this.squares.forEach(square => {
+      square.addEventListener('click', function handleClick(event) {
+        console.log('square clicked', event);
+        square.setAttribute('style', 'border-color: #e74c3c; border-style: solid; border-width: 4pt;');
+      })
+    })
+  }
+
+  createPiece(color) {
+    let piece = document.createElement('div');
+    piece.classList.add('piece');
+    piece.classList.add(color);
+    return piece;
+  }
+
+  createPieces() {
+    this.setupRedPieces();
+  }
+
+  setupRedPieces() {
+    for (let i = 1; i < 9; i++) {
+      for (let j = 1; j < 9; j++) {
+        if ((i*j-1) % 2 === 0) {
+          this.squares.item(i+j).appendChild(this.createPiece('red'));
+        }
+      }
+    }
+  }
+
 }
 
-squares.forEach(square => {
-  square.addEventListener('click', function handleClick(event) {
-    console.log('square clicked', event);
-    highlightSquare(square);
-  })
-})
+const board = new Board(8);
+
+
