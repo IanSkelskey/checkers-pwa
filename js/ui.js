201fd7f932ce1eb34526ce69ab1d155ff0290793
Ian Skelskey <ianskelskey@gmail.com>
2022-10-09 15:10:05 -0400

    Create game logic class

--- js/ui.js
+++ js/ui.js
@@ -1,19 +1,5 @@
-import {GameBoard, XLabel, YLabel} from "./game/game-board.js";
+import {GameLogic} from "./game/game-logic.js";
 
-customElements.define("game-board", GameBoard);
-customElements.define("x-label", XLabel);
-customElements.define("y-label", YLabel);
+const game = new GameLogic();
 
-const main = document.getElementsByTagName("main").item(0);
-const board = document.createElement("game-board");
-const xLabel = document.createElement("x-label");
-const yLabel = document.createElement("y-label");
-
-board.populateTiles();
-board.setupPieces('red');
-board.setupPieces('blue');
-yLabel.populate();
-main.appendChild(yLabel);
-main.appendChild(board);
-xLabel.populate();
-main.appendChild(xLabel);
+game.newGame();
