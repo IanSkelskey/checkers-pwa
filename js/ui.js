6e57f9251e9933c5fb2fd0c96a69ee8df7cf3b21
Ian Skelskey <ianskelskey@gmail.com>
2022-10-09 04:52:05 -0400

    Implement object oriented design and create custom elements

--- js/ui.js
+++ js/ui.js
@@ -1,72 +1,68 @@
-const board = document.getElementById("board");
-
-createBoard();
-
-const squares = document.querySelectorAll(".square")
-
-function highlightSquare(square) {
-  square.setAttribute('style', 'border-color: #e74c3c; border-style: solid; border-width: 4pt;');
-}
-
-function createSquare(color, row, col) {
-  let square = document.createElement('div');
-  square.classList.add('square');
-  square.classList.add(color);
-  square.id = "r" + row + "c" + col;
-  return square;
-}
-
-function createBoard() {
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if ((i+j) % 2 === 0) {
-        board.appendChild(createSquare('black', i, j));
-      } else {
-        board.appendChild(createSquare('white', i, j));
-      }
+class GameBoard extends HTMLElement {
+    constructor() {
+        console.log("Creating game board...")
+        super();
     }
-  }
-  createPieces();
-  console.log('Board created!');
-}
 
-function createPiece(color) {
-  let piece = document.createElement('div');
-  piece.classList.add('piece');
-  piece.classList.add(color);
-  return piece;
-}
-
-function createPieces() {
-  setupRedPieces();
-  setupBluePieces();
-}
-
-function setupRedPieces() {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 8; j++) {
-      if ((i+j) % 2 === 0) {
-        let targetSquare = document.querySelector('#r' + i + 'c' + j);
-        targetSquare.appendChild(createPiece('red'));
-      }
+    populateTiles() {
+        console.log("Populating tiles...");
+        let size = 8;
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                let color = ((i + j) % 2 === 0) ? 'black' : 'white';
+                let tile = document.createElement("board-tile");
+                tile.setProperties(color, i, j);
+                this.appendChild(tile);
+            }
+        }
     }
-  }
-}
 
-function setupBluePieces() {
-  for (let i = 5; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if ((i+j) % 2 === 0) {
-        let targetSquare = document.querySelector('#r' + i + 'c' + j);
-        targetSquare.appendChild(createPiece('blue'));
-      }
+    getTile(row, col) {
+        return this.querySelector('#r' + row + 'c' + col);
     }
-  }
+
+    setupPieces(color) {
+        let startRow = (color === 'red') ? 0 : 5;
+        let endRow = (color === 'red') ? 3 : 8;
+        for (let i = startRow; i < endRow; i++) {
+            for (let j = 0; j < 8; j++) {
+                if ((i + j) % 2 === 0) {
+                    let targetTile = this.getTile(i, j);
+                    let piece = document.createElement('game-piece');
+                    piece.setColor(color);
+                    targetTile.appendChild(piece);
+                }
+            }
+        }
+    }
 }
 
-squares.forEach(square => {
-  square.addEventListener('click', function handleClick(event) {
-    console.log('square clicked', event);
-    highlightSquare(square);
-  })
-})
+class BoardTile extends HTMLElement {
+    constructor() {
+        super();
+    }
+    setProperties(color, row, col) {
+        this.classList.add(color);
+        this.id = "r" + row + "c" + col;
+    }
+}
+
+class GamePiece extends HTMLElement {
+    constructor(color) {
+        super();
+    }
+    setColor(color) {
+        this.classList.add(color);
+    }
+}
+
+customElements.define("game-board", GameBoard);
+customElements.define("board-tile", BoardTile)
+customElements.define("game-piece", GamePiece)
+
+const main = document.getElementsByTagName("main").item(0);
+const board = document.createElement("game-board");
+board.populateTiles();
+board.setupPieces('red');
+board.setupPieces('blue');
+main.appendChild(board);
